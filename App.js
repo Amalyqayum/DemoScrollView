84da605f2c9a6f33c97f4c1a8055ffd2963d08d9
18016222 <amalyqayum@gmail.com>
2020-02-10 23:26:48 +0800

    submission

--- App.js
+++ App.js
@@ -1,9 +1,80 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Image, StyleSheet, View, TouchableHighlight} from 'react-native';
+import {
+  Body,
+  Card,
+  CardItem,
+  Container,
+  Content,
+  Header,
+  Icon,
+  Text,
+  Left,
+  Right,
+} from 'native-base';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      house: '',
+      name: '',
+    };
+  }
+
+  GenerateRandom = () => {
+    fetch('http://hp-api.herokuapp.com/api/characters')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let randomNumber = Math.floor(Math.random() * data.length);
+
+        this.setState({image: data[randomNumber].image});
+        this.setState({house: data[randomNumber].house});
+        this.setState({name: data[randomNumber].name});
+      });
+  };
+
   render() {
-    return <></>;
+    return (
+      <Container>
+        <Header style={{backgroundColor: '#740001'}}>
+          <Text style={{color: '#EEBA30', fontSize: 24}}>
+            FANOID: Harry Potter
+          </Text>
+        </Header>
+        <Content style={{padding: 10}}>
+          <View style={styles.buttonLayout}>
+            <TouchableHighlight
+              style={styles.button}
+              onPress={() => this.GenerateRandom()}>
+              <Text style={styles.buttonText}>Random Character</Text>
+            </TouchableHighlight>
+          </View>
+          <Card>
+            <CardItem cardBody style={{flexDirection: 'row'}}>
+              <Image
+                style={styles.imageStyle}
+                source={{uri: this.state.image}}
+              />
+              <CardItem style={{flexDirection: 'column', flex: 1}}>
+                <Text>{this.state.name}</Text>
+                <Text>{this.state.house}</Text>
+              </CardItem>
+            </CardItem>
+            <CardItem footer>
+              <Left />
+              <Body />
+              <Right style={{flexDirection: 'row', marginLeft: 50}}>
+                <Icon name="star" />
+                <Text style={{marginLeft: 5}}>Favourite</Text>
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
   }
 }
 
@@ -12,6 +83,26 @@ const styles = StyleSheet.create({
     height: 200,
     width: null,
     flex: 1,
+  },
+
+  button: {
+    width: 300,
+    height: 60,
+    backgroundColor: '#EEBA30',
+    borderRadius: 30,
+    justifyContent: 'center',
+  },
+
+  buttonLayout: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 50,
+  },
+
+  buttonText: {
+    alignSelf: 'center',
+    color: '#740001',
   },
 });
 
--- Worksheet.js
+++ Worksheet.js
@@ -1,8 +1,49 @@
 import React, {Component} from 'react';
 import {Image, View} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  Text,
+  Body,
+  CardItem,
+  Card,
+  Icon,
+} from 'native-base';
 
 export default class Worksheet extends Component {
   render() {
-    return <></>;
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card>
+            <CardItem header>
+              <Text style={{fontWeight: 'bold'}}>View Cart</Text>
+            </CardItem>
+            <CardItem>
+              <Image
+                style={{height: 200, width: null, flex: 1}}
+                source={require('./src/images/cart.jpg')}
+              />
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Total items: 7</Text>
+                <Text>Expected Amount: $400</Text>
+                <View Style={{flexDirection: 'row', marginTop: 20}} />
+                <Icon name="mail" />
+                <Text>Send to Mail</Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text style={{fontWeight: 'bold'}}>Make Payment</Text>
+            </CardItem>
+          </Card>
+        </Content>
+        <Footer />
+      </Container>
+    );
   }
 }
